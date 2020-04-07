d1ae7d49b2cadc2d0f29eb28481f7b0202a392a7
JayWil19 <56897171+JayWil19@users.noreply.github.com>
2020-04-07 14:56:19 -0400

    Fixing It
    Everything got messed up in final merge. had to go through it to make sure everything was good

--- FlashCardApp/src/flashcardapp/FlashCardGUI.java
+++ FlashCardApp/src/flashcardapp/FlashCardGUI.java
@@ -14,6 +14,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.JOptionPane;
 
 /**
